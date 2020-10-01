8c70d5e41d4e4c782df1e23dbbc719a53ae410e0
KotaAsami <31060@ojs.ac.jp>
2020-10-01 10:38:00 +0900

    P117 リスト4-28

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Chapter4 {
     class Program {
         static void Main(string[] args) {
-            string code = "12345";
+            #region null合体演算子
+            /*    string code = "12345";
 
             var message = GetMessage(code) ?? DefaultMessage();
             Console.WriteLine(message);
@@ -19,6 +22,29 @@ namespace Chapter4 {
 
         private static object GetMessage(string code) {
             return 123;
+        }*/
+            #endregion
+            #region null条件式
+            Console.WriteLine(GetProduct());
+        }
+        private static string GetProduct() {
+            Sale sale = new Sale {
+                ShopName = "pet store",
+                Amount = 100000,
+                Product = "food"
+            };
+            sale = null;
+            return sale?.Product;
+            
+        }
+        #endregion
+
+        class Sale {
+            //店舗名
+            public string ShopName { get; set; }
+            //売上高
+            public int Amount { get; set; }
+            public string Product { get; set; }
         }
     }
 }
